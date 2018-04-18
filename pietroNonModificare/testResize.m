729d9ccc8b49edbd4b52b3e0c5748d3ac63ed212
piepari <p.parini2@campus.unimib.it>
2018-04-18 23:25:33 +0200

    aggiunta funzione più script di test per binarizzazione con otsu

--- pietroNonModificare/testResize.m
+++ pietroNonModificare/testResize.m
@@ -1,14 +1,24 @@
 %% calsse di test per la funzione di resize
 %i title vanno quando vogliono loro, non farci caso
 
-imageTest = imread('immaginiScacchi/004.jpg');
-[x,y,~] = size (imageTest);
+a=1;
+b=6;
+images = readImages(a,b);
+test= zeros(1, (b-a+1));
 
-figure, imshow(imageTest), title(strcat((num2str(x)), 'x',(num2str(y))));
-[imageAfterResize, scale]= resize(imageTest);
-[m,n,~] = size (imageAfterResize);
 
-figure, imshow(imageAfterResize), title(strcat('scale :', num2str(scale)));
 
-%se la variabile test è true sta funzionando tutto bene
-test=((m * scale >= (x-1))&&(m * scale <= (x+1)));
+for i=1:(b-a+1)
+
+    imageTest = images{i};
+    [x,y,~] = size (imageTest);
+
+    figure, imshow(imageTest), title(strcat((num2str(x)), 'x',(num2str(y))));
+    [imageAfterResize, scale]= resize(imageTest);
+    [m,n,~] = size (imageAfterResize);
+
+    figure, imshow(imageAfterResize), title(strcat('scale :', num2str(scale)));
+
+    %ogni cella dell'array di test deve essere a true 
+    test(i)=((m * scale >= (x-1))&&(m * scale <= (x+1)));
+end
