7f9296a787b5d39e4b36e241350849d9099b92cd
piepari <p.parini2@campus.unimib.it>
2018-06-08 12:01:49 +0200

    add functionfor correlation chessboard

--- isChessboard.m
+++ isChessboard.m
@@ -2,11 +2,18 @@ function flag = isChessboard(testChessboard)
 
  testChessboard =rgb2gray(testChessboard);
  test= imread('./Foto/test/test1.jpg');
- [x,y,channel]=size(test);
-  if channel > 1
-     test = rgb2gray (test);
-  end
+ [x,y,~]=size(test);
  
+  test =rgb2gray (test);
+  test=imbinarize(test);
+  
+  testChessboard=imbinarize(testChessboard);
+  
+  test=imopen(test , strel ('disk', 3));
+  testChessboard= imopen(testChessboard , strel ('disk', 3));
+  
+  figure, subplot(1,2,1), imshow(test)
+          subplot(1,2,2), imshow(testChessboard);
  
  
  
