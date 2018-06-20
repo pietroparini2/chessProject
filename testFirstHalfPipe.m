eb7673f203017586befc0f33733d759c3d4a0328
piepari <p.parini2@campus.unimib.it>
2018-06-20 16:41:22 +0200

    time

--- testFirstHalfPipe.m
+++ testFirstHalfPipe.m
@@ -5,6 +5,7 @@ b=60;
 images = readImages(a,b);
 test= zeros(1, (b-a+1));
 elaboration=zeros(1, (b-a+1));
+time=0;
 
 
 for i=1:(b-a+1)
@@ -15,8 +16,11 @@ for i=1:(b-a+1)
     [imageTest, choose]= chooseElaboration(imageResized,scale,original);
     t1 = etime(clock,t0);
     fprintf('immagine: %d \n elaborazione usata: %d \n in tempo %.2f seconds.\n\n',i, choose,  t1);
+    time=time+t1;
     elaboration(i)=choose;
     figure, subplot(1,2,1), imshow(original),
             subplot(1,2,2), imshow(imageTest),
             
 end
+time= time/(b-a+1);
+fprintf('media dei tempi di elaborazine: %.2f secondi \n', time);
